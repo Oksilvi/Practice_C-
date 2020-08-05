9e2ac95df2f4cdecd493ffbffeb9d0e6433ca698
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-08-05 19:41:36 +0300

    8th task

---
+++ SB8_10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Input");
+		string x = "Geranium", y = "Violet", z = "Cactus", k = "";
+		int N, i;
+		N = Convert.ToInt32(Console.ReadLine());
+		for (i = 1; i <= N; i = i + 1)
+		{
+			k = z;
+			z = y;
+			y = k;
+			k = x;
+			x = y;
+			y = k;
+		}
+		Console.WriteLine(x + " " + y + " " + z);
+		Console.ReadKey();
+	}
+}
