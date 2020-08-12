73229273e95ad6e47122dbd9c19324149c16e817
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-08-12 22:34:13 +0300

    9th task: Multiplication table

---
+++ SB9_7.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i, k;
+		for( i = 1; i <= 9; i ++ )
+		{
+			for( k = 1; k <= 9; k ++ )	     
+		Console.WriteLine(i + " " + "x" + " " + k + " " + "=" + " " + i * k);
+		}
+	}
+}
