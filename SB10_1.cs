7353f86080c5040b2238925d43751061b24db525
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-08-14 13:06:42 +0300

    10th task: Input hello through WHILE

---
+++ SB10_1.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int N;
+		N = 0;
+		do 
+		{
+			N ++;
+			Console.WriteLine("Hello");
+		}
+		while( N < 3 );
+	}
+}
