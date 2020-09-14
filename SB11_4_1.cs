9e20528fb94400aef4a24301ae11f543730529a7
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-09-14 23:04:32 +0300

    11th task: Array of random numbers + their summa and mean

--- SB11_4_1.cs
+++ SB11_4_1.cs
@@ -1,14 +1,20 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
 	{
-		int [] array = new int [100];
+		int[] array = new int[100];
+		Random rand = new Random();
+		int sum = 0, R = 0;
+		double m = 0;
+		for (int i = 0; i < 100; i++)
 		{
-			Random rand = new Random();
-		        for( int i = 0; i < 100; i ++ )
-				Console.WriteLine(rand.Next(50) + 1);
+			R = rand.Next(50) + 1;
+			sum += R;
+			m = i;
+			Console.WriteLine(R);
 		}
+		Console.WriteLine("summa:" + " " + sum + " " + "mean" + " " + sum / (m + 1));
 	}
 }
