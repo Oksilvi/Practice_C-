c97e3e22ad07e9425b81abf374063dae195d958f
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-09-16 15:27:44 +0300

    11th task: Finding a value in an array

--- SB11_4_2.cs
+++ SB11_4_2.cs
@@ -1,22 +1,22 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
 	{
 		Console.WriteLine("Input:");
 		int N = Convert.ToInt32(Console.ReadLine());
-		int R = 0, k = 0;
-		int [] array = new int[15];
+		int k = 0;
+		int[] array = new int[15];
 		Random rand = new Random();
-		for( int i = 0; i < 15; i++ )
+		for (int i = 0; i < 15; i++)
 		{
-			k ++;
-			R = rand.Next(10) + 1;
-			Console.WriteLine(R);
-			if( N == R ) break;
+			k++;
+			array[i] = rand.Next(10) + 1;
+			Console.WriteLine(array[i]);
+			if (N == array[i]) break;
 		}
-		if( N == R ) Console.WriteLine("Line:" + " " + k); else
+		if (k < 15) Console.WriteLine("Line:" + " " + k); else
 		Console.WriteLine("No values");
 	}
 }
