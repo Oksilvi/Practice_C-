92549c4c109053d50c4fa52a17ebb4f027548aec
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-09-21 13:55:11 +0300

    11th task: Finding the Min and Max in an array

--- SB11_4_3.cs
+++ SB11_4_3.cs
@@ -1,21 +1,25 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
 	{
-		int [] array = new int[15];
+		int S = Convert.ToInt32(Console.ReadLine()); // array size
+		int[] array = new int[S];
 		Random rand = new Random();
 		array[0] = rand.Next(30) + 1;
 		int max = array[0], min = array[0];
-		for( int i = 0; i < 15; i ++ )
+		for( int i = 0; i < S; i++ )
 		{
 			array[i] = rand.Next(30) + 1;
 			Console.WriteLine(array[i]);
+		}
+		for( int i = 0; i < S; i++ )
+		{
 			if( array[i] > max )
-			    max = array[i];
-			if( array[i] < min ) 
-			    min = array[i];
+				max = array[i];
+			if( array[i] < min )
+				min = array[i];
 		}
 		Console.WriteLine("Min:" + " " + min + " " + "Max:" + " " + max);
 	}
