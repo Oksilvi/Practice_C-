a31d39c0eb16e49fa17d0dfe6f3e1c16ba88a770
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-09-21 14:03:01 +0300

    11th task: Second max in the array

--- SB11_4_4.cs
+++ SB11_4_4.cs
@@ -1,26 +1,30 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
 	{
-		int [] array = new int[10];
+		int S = Convert.ToInt32(Console.ReadLine()); // array size
+		int [] array = new int[S];
 		Random rand = new Random();
 		array[0] = rand.Next(30) + 1;
 		int max = array[0], max2 = 1;
 		Console.WriteLine(array[0]);
-		for( int i = 1; i < 10; i ++ )
+		for( int i = 1; i < S; i ++ )
 		{
 			array[i] = rand.Next(30) + 1;
 			Console.WriteLine(array[i]);
+		}
+		for( int i = 1; i < S; i ++ )
+		{
 			if( array[i] > max )
-			{ 
-			    max2 = max;
-			    max = array[i];
+			{
+				max2 = max;
+				max = array[i];
 			}
 			else
 			if( array[i] <= max && array[i] > max2 )
-			    max2 = array[i];
+				max2 = array[i];
 		}
 		Console.WriteLine("Max:" + " " + max + " " + "Max2:" + " " + max2);
 	}
