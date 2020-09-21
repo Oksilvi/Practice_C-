318a7599f33f81ff08c9e9d32ee23c863ee81ea2
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-09-22 00:06:51 +0300

    11th task: Copying an array

--- SB11_4_6.cs
+++ SB11_4_6.cs
@@ -1,25 +1,28 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
 	{
 		Console.WriteLine("Input:");
-		int N;
-		N = Convert.ToInt32(Console.ReadLine());
-		int [] array = new int[N];
-		int [] array2 = new int[2 * N];
+		int S = Convert.ToInt32(Console.ReadLine());
+		int [] array = new int[S];
+		int [] array2 = new int[2 * S];
 		Random rand = new Random();
-		for( int i = 0; i < N; i ++ )
+		for( int i = 0; i < S; i ++ )
 		{
 			array[i] = rand.Next(25) + 1;
 			Console.WriteLine(array[i]);
-			if( i == N - 1 )
+		}
+		Console.WriteLine("Copy:");
+		for( int i = 0; i < S; i ++ )
+		{
+			if( i == S - 1 )
 			{
-				for( int k = i; k < 2 * i + 1; k ++ )
+				for( int k = i; k < 2 * S - 1; k ++ )
 				{
-			             array2[k] = array[k - i];	
-			             Console.WriteLine(array2[k]);
+					array2[k] = array[k - i];
+					Console.WriteLine(array2[k]);
 				}
 			}
 		}
