69199aa74cb5ad5aff8a24c6e12849bf9534515e
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-09-22 23:18:35 +0300

    11th task: Finding the same values

---
+++ SB11_4_7.cs
@@ -0,0 +1,38 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Input:");
+		int S = Convert.ToInt32(Console.ReadLine()); // array size
+		int [] array1 = new int[S];
+		int [] array2 = new int[S];
+		Random rand = new Random();
+		Console.WriteLine("Array1:");
+		for( int i = 0; i < S; i ++ )
+		{
+			array1[i] = rand.Next(5) + 1;
+			Console.WriteLine(array1[i]);
+		}
+		Console.WriteLine("Array2:");
+		for( int k = 0; k < S; k ++ )
+		{
+			array2[k] = rand.Next(5) + 1;
+			Console.WriteLine(array2[k]);
+		}
+		Console.Write("The same:");
+		for( int i = 0; i < S; i ++ )
+		{
+			for( int k = 0; k < S; k ++ )
+		    {
+			  if( array2[k] == array1[i] )
+			  {
+			      Console.Write( array2[k] + " " );
+				  array1[i] = 0;
+				  array2[k] = S + 1;
+			  }
+			}
+		} 
+	}
+}
