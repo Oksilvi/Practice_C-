9bf6f3c4589121c510170b2a17b6cb644510c76c
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-09-24 20:57:25 +0300

    11th task: Bubble sort algorithm

---
+++ SB11_4_8.cs
@@ -0,0 +1,29 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+	        int[] array = new int[10];
+	        Random rand = new Random();
+		for (int i = 0; i < 10; i++)
+		{
+		     array[i] = rand.Next(20) + 1;
+		     Console.WriteLine(array[i]);
+		}
+		Console.WriteLine("Bubble sort algorithm:");
+		for( int i = 0; i < 10; i ++ )
+		{	
+		     for( int k = i + 1; k < 10; k ++ )
+		     {
+		          if( array[k] < array[i] )
+			  {
+			      array[i] = array[k] - array[i];
+			      array[k] = array[k] - array[i];
+			      array[i] = array[i] + array[k];
+			   }
+		      }
+		      Console.WriteLine(array[i]);
+		}
+	}
+}
