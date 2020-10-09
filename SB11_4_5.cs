310ccffd3a5e3fc787de82f3596612388adfc47a
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-10-09 16:37:10 +0300

    11th task: Reverse array

--- SB11_4_5.cs
+++ SB11_4_5.cs
@@ -8,10 +8,20 @@ public class Program
 		int S = Convert.ToInt32(Console.ReadLine()); // array size
 		int [] array = new int[S];
 		Random rand = new Random();
-		Console.WriteLine("Mirror:");
-		for( int i = S - 1; i >= 0; i -- )
+		for( int i = 0; i < S; i ++ )
 		{
 			array[i] = rand.Next(25) + 1;
+			Console.WriteLine(array[i]);
+		}
+		Console.WriteLine("Reverse array:");
+		for( int i = 0; i < S / 2; i ++ )
+		{
+			int t = array[i];
+			array[i] = array[S - 1 - i];
+			array[S - 1 - i] = t;
+		}
+		for( int i = 0; i < S; i ++ )
+		{
 			Console.WriteLine(array[i]);
 		}
 	}
