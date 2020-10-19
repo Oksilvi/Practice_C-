eceb7ff05baa9c4bb67e555da5e566383503173a
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-10-19 17:56:38 +0300

    12th task: Compare 3D arrays

--- SB12_4_2.cs
+++ SB12_4_2.cs
@@ -1,5 +1,5 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
@@ -11,47 +11,48 @@ public class Program
 		int L2 = Convert.ToInt32(Console.ReadLine()); // measurement 2.2
 		int M2 = Convert.ToInt32(Console.ReadLine()); // measurement 2.3
 		Random rand = new Random();
-		int [,,] array1 = new int[K1,L1,M1];
-		int [,,] array2 = new int[K2,L2,M2];
-		for( int x = 0; x < K1; x ++ ) 
+		int[,,] array1 = new int[K1, L1, M1];
+		int[,,] array2 = new int[K2, L2, M2];
+		for (int x = 0; x < K1; x++)
 		{
-			for( int y = 0; y < L1; y ++ )
-			{	
-				for( int z = 0; z < M1; z ++ )
+			for (int y = 0; y < L1; y++)
+			{
+				for (int z = 0; z < M1; z++)
 				{
-		             array1[x,y,z] = rand.Next(15) + 1;
+					array1[x, y, z] = rand.Next(15) + 1;
 				}
 			}
 		}
-		for( int x = 0; x < K1; x ++ ) 
+		for (int x = 0; x < K1; x++)
 		{
-			for( int y = 0; y < L1; y ++ )
-			{	
-				for( int z = 0; z < M1; z ++ )
+			for (int y = 0; y < L1; y++)
+			{
+				for (int z = 0; z < M1; z++)
 				{
-		             Console.Write(array1[x,y,z] + " ");
+					Console.Write(array1[x, y, z] + " ");
 				}
 				Console.WriteLine();
 			}
 			Console.WriteLine(" ");
 		}
-		if( K1 == K2 && L1 == L2 && M1 == M2 )
+
+		if (K1 == K2 && L1 == L2 && M1 == M2)
 		{
-		   for( int x = 0; x < K2; x ++ ) 
-		   {
-			   for( int y = 0; y < L2; y ++ )
-			   {	
-				   for( int z = 0; z < M2; z ++ )
-				   {
-		                array2[x,y,z] = rand.Next(15) + 1;
-					    if( array1[x,y,z] != array2[x,y,z] )
+			for (int x = 0; x < K2; x++)
+			{
+				for (int y = 0; y < L2; y++)
+				{
+					for (int z = 0; z < M2; z++)
+					{
+						array2[x, y, z] = rand.Next(15) + 1;
+						if (array1[x, y, z] != array2[x, y, z])
 						{
 							Console.WriteLine("Arrays are not equal");
 							return;
 						}
-				   }
-			   }
-		    }
+					}
+				}
+			}
 			Console.WriteLine("Arrays are equal");
 		}
 		else Console.WriteLine("Arrays are not equal");
