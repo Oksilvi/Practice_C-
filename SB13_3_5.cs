6ad050677d1e4bb0dfb66ae6615169abc6bceb7b
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-10-27 18:23:51 +0300

    13th task: String interpolation

---
+++ SB13_3_5.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB13_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		string s1 = Convert.ToString(Console.ReadLine());
+		string s2 = Convert.ToString(Console.ReadLine());
+		string s3 = $"{s1} {s2}";
+		Console.Write(s3);
+		Console.ReadKey();
+	}
+    }
+}
