2cac20324b4f32f6e6630af111e0d2ab41c2c2bf
Oksilvi <oksana.siliverstova.98@gmail.com>
2020-11-03 14:37:16 +0300

    13th task: string comparison

---
+++ SB13_3_6_1.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string s1 = Convert.ToString(Console.ReadLine());
+		string s2 = Convert.ToString(Console.ReadLine());
+		Console.WriteLine(s1.Contains(s2));
+	}
+}
